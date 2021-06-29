a8bbe54d64f48de1b54903aefcb97b9e511831e3
Bersuc <dbersuc@gmail.com>
2021-06-28 21:19:01 -0300

    Ajustando caso seja enviado o Indice Bovespa

--- post/status.js
+++ post/status.js
@@ -5,7 +5,13 @@
 function setStatus(screen_name, price, symbol, prcFlcn) {
   let floatingStatus = setReplyChange(prcFlcn);
   let username = `@${screen_name}`;
-  let status = `@${screen_name} Olá ${screen_name}, o ativo ${symbol} está cotado a R$ ${price}*, ${floatingStatus}\nfonte: B3\n*Uso apenas informativo, com atraso e em testes.`;
+  let status = "";
+  if (symbol == "IBOV") {
+    status = `@${screen_name} Olá ${screen_name}, o Ibovespa está com ${price}* pontos, ${floatingStatus}\nfonte: B3\n*Uso apenas informativo, com atraso e em testes.`;
+  } else {
+    status = `@${screen_name} Olá ${screen_name}, o ativo ${symbol} está cotado a R$ ${price}*, ${floatingStatus}\nfonte: B3\n*Uso apenas informativo, com atraso e em testes.`;
+  }
+
   return { status, username };
 }
 
